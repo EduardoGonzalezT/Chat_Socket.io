bea28a9bb25a2287b0690f1f7f22622ff033e09b
Eduardo <eduardo-gonzalez50@hotmail.com>
2023-10-05 16:47:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//server config
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// settings
+app.set('port' , process.env.PORT || 3000);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// start server
+const server = app.listen(app.get('port') , () => {
+    console.log('server on port', app.get('port'));
+})
+
+// websockets
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection' , (socket) => {
+    console.log('new connection' , socket.id)
+});
+
